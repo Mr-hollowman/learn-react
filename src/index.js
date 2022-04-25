cbd7d12325e1828af4c855798e3654483cbbbca5
Mr-hollowman <selvamani825@gmail.com>
2022-04-25 20:23:18 +0530

    git add -u main

--- src/index.js
+++ src/index.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App';
-import Example from './protype/example';
+import App from './App'
 
-ReactDOM.render(<App/>,document.getElementById('root'));
+ReactDOM.render(<App />,document.getElementById('root'));
